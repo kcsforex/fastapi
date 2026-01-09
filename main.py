02a7081554066c4164333f25a8d3c370f45c04e7
kcsforex <110934502+kcsforex@users.noreply.github.com>
2026-01-10 00:36:31 +0100

    Update main.py

--- main.py
+++ main.py
@@ -39,7 +39,7 @@ SIDEBAR_STYLE = {
     "backdrop-filter": "blur(15px)",
     "border-radius": "20px",
     "border": "1px solid rgba(255, 255, 255, 0.1)",
-    "box-shadow": "0 8px 32px 0 rgba(0, 0, 0, 0.37)",
+    "box-shadow": "0 8px 32px 0 rgba(0, 0, 0, 0.5)",
     "z-index": 1000,
 }
 
