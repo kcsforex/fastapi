337b1558b1238b87f817b93bf2ab0a37569dea4f
kcsforex <110934502+kcsforex@users.noreply.github.com>
2026-01-11 21:50:00 +0100

    Update main.py

--- main.py
+++ main.py
@@ -14,7 +14,7 @@ app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.DARKL
 
 # ----- 2. NOW IMPORT YOUR PAGES -----
 # By importing them here, 'app' already exists when dash.register_page is called
-from pages import home, crypto, crypto0,  ml-databricks, ml-databricks1 # This triggers register_page correctly
+from pages import home, crypto, crypto0,  ml-databricks, ml-databricks1
 
 # ----- 3. FASTAPI WRAPPER -----
 server = FastAPI(title="Dash Main App")
