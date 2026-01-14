9b0f5644fda11cbfe4b3b3163d9f5c4322857f5e
kcsforex <110934502+kcsforex@users.noreply.github.com>
2026-01-14 21:20:03 +0100

    Update crypto.py

--- pages/crypto.py
+++ pages/crypto.py
@@ -110,7 +110,9 @@ def update_dashboard(n):
     if df.empty:
         return dash.no_update, "No data found", {}, "No Data"
         
-    latest = df.sort_values("timestamp").groupby("symbol", as_index=False).tail(1) #.assign(symbol=lambda x: x["symbol"].str.lower()).set_index("symbol")
+    latest = df.sort_values("timestamp").groupby("symbol").last().reset_index() #as_index=False).tail(1) #.assign(symbol=lambda x: x["symbol"].str.lower()).set_index("symbol")
+
+    #summary = latest[['symbol', 'price', 'sma_100', 'percent_diff', 'price_status', 'price_cross', 'timestamp']]
 
     # 0. Update Timestamp
     metrics_update = f"Updated -> {pd.to_datetime(latest["timestamp"].iloc[0],unit="ms").strftime("%Y-%m-%d %H:%M")}"
