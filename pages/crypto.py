1ceea5e8054092d955897afb1308f85adc55c0d2
kcsforex <110934502+kcsforex@users.noreply.github.com>
2026-01-15 13:06:27 +0100

    Update crypto.py

--- pages/crypto.py
+++ pages/crypto.py
@@ -109,12 +109,12 @@ def update_dashboard(n):
     if df.empty:
         return dash.no_update, "No data found", {}, "No Data"
 
-    df["timestamp"] = pd.to_datetime(df["timestamp"],unit="ms").dt.strftime("%Y-%m-%d %H:%M:%S")
+    df["timestamp"] = pd.to_datetime(df["timestamp"],unit="ms").dt.strftime("%Y-%m-%d %H:%M")
         
     latest = df.sort_values("timestamp").groupby("symbol").last().reset_index() 
 
     # 0. Update Timestamp
-    metrics_update = f"Updated -> {latest["timestamp"].iloc[0].strftime("%Y-%m-%d %H:%M")}"
+    metrics_update = f"Updated -> {latest["timestamp"].iloc[0]}"
     # metrics_update = pd.to_datetime(latest.loc["btc", "timestamp"],unit="ms").strftime("%Y-%m-%d %H:%M")
              
     # 1. Create Top Metrics (Quick visual check)
