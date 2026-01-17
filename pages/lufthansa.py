6880c52d1559522a9f0408fbea1acebc210d609f
kcsforex <110934502+kcsforex@users.noreply.github.com>
2026-01-17 20:56:03 +0100

    Update lufthansa.py

--- pages/lufthansa.py
+++ pages/lufthansa.py
@@ -179,11 +179,11 @@ layout = dbc.Container([
 )
 
 def update_dashboard(n):
-    conn1 = psycopg2.connect(DB_CONFIG)
-    df = pd.read_sql("SELECT * FROM lh_flights ORDER BY timestamp DESC LIMIT 120", conn1)
-    conn1.close()
-    if df.empty:
-        return dash.no_update, "No data found", {}, "No Data"
+    #conn1 = psycopg2.connect(DB_CONFIG)
+    #df = pd.read_sql("SELECT * FROM lh_flights ORDER BY timestamp DESC LIMIT 120", conn1)
+    #conn1.close()
+    #if df.empty:
+    #    return dash.no_update, "No data found", {}, "No Data"
 
     #df["timestamp"] = pd.to_datetime(df["timestamp"],unit="ms", utc=True).dt.tz_convert("Europe/Budapest").dt.strftime("%Y-%m-%d %H:%M:%S")       
     #latest = df.sort_values("timestamp").groupby("symbol").last().reset_index() 
