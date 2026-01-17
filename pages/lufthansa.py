52ad267e95ff25a214cb27eea8f65cde86b74e94
kcsforex <110934502+kcsforex@users.noreply.github.com>
2026-01-17 23:29:20 +0100

    Update lufthansa.py

--- pages/lufthansa.py
+++ pages/lufthansa.py
@@ -188,7 +188,7 @@ def update_dashboard(n_intervals):
         return html.Div("No data found", className="text-light fst-italic")
 
     #df["ingested_at"] = pd.to_datetime(df["ingested_at"],unit="ms", utc=True).dt.tz_convert("Europe/Budapest").dt.strftime("%Y-%m-%d %H:%M:%S")  
-    #df["ingested_at"] = pd.to_datetime(df["ingested_at"])
+    df["ingested_at"] = pd.to_datetime(df["ingested_at"])
     df["ingested_at"] = df["ingested_at"].dt.tz_convert("Europe/Budapest").dt.strftime("%Y-%m-%d %H:%M:%S")
 
     # 0. Update Timestamp
