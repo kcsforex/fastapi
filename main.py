8b75b041bdc5188c9ff0c5fe8560ada7a5e6d128
kcsforex <110934502+kcsforex@users.noreply.github.com>
2026-01-18 12:48:16 +0100

    Update main.py

--- main.py
+++ main.py
@@ -9,8 +9,9 @@ from fastapi.middleware.wsgi import WSGIMiddleware
 app = dash.Dash(__name__, use_pages=True, external_stylesheets=[dbc.themes.DARKLY, "https://use.fontawesome.com/releases/v5.15.4/css/all.css"])
 
 # ----- 2. NOW IMPORT YOUR PAGES -----
-from pages import home, home0, crypto, crypto0,  ml_databricks, air_dataset, lufthansa
+from pages import home, crypto,  ml_databricks, air_dataset, lufthansa
 import apis.lufthansa_api as lufthansa
+import apis.crypto_api as crypto
 
 # ----- 3. FASTAPI WRAPPER -----
 server = FastAPI(title="Dash Main App")
