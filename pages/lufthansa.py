bee0deaa2316f962b7908163aebeab73d795e869
kcsforex <110934502+kcsforex@users.noreply.github.com>
2026-01-19 13:38:11 +0100

    Update lufthansa.py

--- pages/lufthansa.py
+++ pages/lufthansa.py
@@ -6,7 +6,7 @@ import dash_bootstrap_components as dbc
 import plotly.express as px 
 import plotly.graph_objects as go
 from apis.lufthansa_api import sql_engine 
-import flight_ml as fml
+from pages.flight_ml import fml
 
 dash.register_page(__name__, icon="fa-plane", name="Lufthansa Tracker")
 
