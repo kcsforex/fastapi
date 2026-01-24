5439c107e89a4ab9da8cb41f0352b1626087f9de
kcsforex <110934502+kcsforex@users.noreply.github.com>
2026-01-24 18:03:32 +0100

    Update crypto_api.py

--- apis/crypto_api.py
+++ apis/crypto_api.py
@@ -12,7 +12,7 @@ from sqlalchemy import create_engine
 
 # ----- 1. CONFIGURATION -----
 DB_CONFIG = "postgresql://sql_admin:sql_pass@72.62.151.169:5432/n8n"
-sql_engine = create_engine(DB_CONFIG, pool_size=10, max_overflow=20)
+sql_engine = create_engine(DB_CONFIG, pool_size=0, max_overflow=0, pool_pre_ping=True)
 SYMBOLS = ["BTC/USDT", "ETH/USDT", "SOL/USDT", "XRP/USDT", "ZEN/USDT", "LTC/USDT", "AVAX/USDT", "LINK/USDT", "HYPE/USDT", "BCH/USDT", "BNB/USDT", "AAVE/USDT"]
 
 # ----- 2. FASTAPI  (n8n targets this) -----
